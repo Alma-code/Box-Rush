728173f83a8a38207260100a4cd67180b153f845
Alma-code <92029000+Alma-code@users.noreply.github.com>
2024-02-15 12:23:36 +0000

    Add files via upload
    The script is attached to an obstacle game object. It deletes the obstacle after the player passes it to ensure that only the obstacles ahead of the player are active.
    This helps to reduce gameplay lag, if there is any.

---
+++ DeleteCollectable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteCollectable : MonoBehaviour {
+	public GameObject Collectable;
+	
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		if (collisionInfo.collider.tag == "Obstacle")
+		{
+		Destroy(Collectable,0.1f);
+		}
+		
+	}
+
+	
+}
