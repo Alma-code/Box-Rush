d7386e37f425ce302286e3c623a019588fea1e54
Alma-code <92029000+Alma-code@users.noreply.github.com>
2024-02-15 10:23:44 +0000

    Add files via upload
    It spawns obstacles into the path of the player. It is attached to a game object that manages the obstacle meshes.

---
+++ ObstacleManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour {
+	public GameObject[] tilePrefabs;
+	private Transform playerTransform;
+	private float spawnZ = -90.22675f;
+	private float tileLength = 154.4535f;
+	private int amnTilesOnScreen = 2;
+	public float  SafeZone = 200.0f;
+	public Vector3 Offset;
+	public List<GameObject> activeTiles;
+	public int LastPrefabIndex = 0;
+	
+	// Use this for initialization
+	void Start () {
+	activeTiles = new List<GameObject>();
+	playerTransform = GameObject.FindGameObjectWithTag("Player").transform;	
+	 
+	 for (int i = 0; i < amnTilesOnScreen; i++)
+	 {
+	 	 SpawnTile ();
+	 }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (playerTransform.position.z - SafeZone > (spawnZ - amnTilesOnScreen * tileLength))
+		{
+			SpawnTile();
+			DeleteTile();
+		}
+	}
+
+	void SpawnTile(int prefabIndex = -1)
+	{
+		GameObject go;
+		go = Instantiate (tilePrefabs [RandomPrefabIndex()]) as GameObject;
+		go.transform.SetParent (transform);
+		go.transform.position = (Vector3.forward * spawnZ) + Offset;
+		spawnZ += tileLength;
+		activeTiles. Add(go);
+	}
+	public void DeleteTile()
+	{
+		Destroy (activeTiles[0]);
+		activeTiles.RemoveAt(0); 
+	}
+	public int RandomPrefabIndex()
+	{
+		if (tilePrefabs.Length <=1)
+			return 0;
+		
+		int randomIndex = LastPrefabIndex;
+		while (randomIndex == LastPrefabIndex)
+			{
+			randomIndex = Random.Range (0, tilePrefabs.Length);
+			}
+			LastPrefabIndex = randomIndex;
+			return randomIndex;
+
+	}
+}
